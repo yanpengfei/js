fdc323cf62fa3e4b853049144b5cafc31b87cc51
闫鹏飞 <yanpengfei@jiashuangkuaizi.com>
2016-03-15 15:06:39 +0800

    项目更新

--- node/child_process/child_process.js
+++ node/child_process/child_process.js
@@ -6,21 +6,24 @@ var server = http.createServer(function (req,res){
 });
 
 var work;
-process.on('message',function (m, tcp){    // 接收主进程发过来的消息
+process.on('message',function (m, tcp){    // 子进程message事件接收主进程发过来的消息，并接收句柄
 	if ( m === 'server' ) {
 		work = tcp;
-		work.on('connection',function (socket){  // 接收句柄
-			server.emit('connection',socket);    // 触发监控
+		work.on('connection',function (socket){  // 句柄connection事件
+			server.emit('connection',socket);    // 手动触发http的connection，并进入解析阶段
 		});
 	}
 });
 
-process.on('uncaughtException', function (){   // 未捕获的异常错误
+process.on('uncaughtException', function (){   // 子进程未捕获的异常错误
+
 	process.send({act:'suicide'});  //   发送给主进程消息
+
 	work.close(function (){			
-		process.exit(1);			// 进程退出
+		process.exit(1);			// 子进程退出
 	});
-	setTimeout(function (){			// 定时退出
+
+	setTimeout(function (){			// 子定时退出
 		process.exit(1);
 	},5000);
 })
---
+++ node/child_process/main_process.js
@@ -0,0 +1,40 @@
+var cp = require('child_process');
+var cpus = require('os').cpus().length;
+
+var server = require('net').createServer();
+server.listen(3000);
+
+var works = {};
+var createWorker = function() {
+	
+	var worker = cp.fork(__dirname+'/child_process.js');  // 创建进程
+
+	worker.on('exit',function(){   // 接收到子进程退出事件
+		console.log('worker '+ worker.pid + ' exited.');
+		delete works[worker.pid];  // 进程维护对象删除
+	});
+
+	worker.on('message', function (m){  // 接到子进程消息
+		if ( m.act === 'suicide' ) {
+			createWorker();
+		}
+	});
+
+	process.on('message',function(m){
+		console.log(m);
+	});
+	worker.send('server',server);  // 发送句柄给子进程
+	works[worker.pid] = worker;    // 添加进程进进程维护对象
+	console.log('Create worker. pid : '+worker.pid);
+
+}
+
+for ( var i = 0; i < cpus; i++ ) {
+	createWorker();   // 根据cpu个数创建进程
+}
+
+process.on('exit',function(){  // 主进程退出事件
+	for ( var pid in workers) {
+		workers[pid].kill();  // 所有子进程退出
+	}
+});
